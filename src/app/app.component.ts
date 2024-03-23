e38f60e9df002ae9f5dda916bbab4fa2f7a01fb3
ritik yadav <ritik.yadav@ridingad.com>
2024-03-24 02:49:04 +0530

    testing carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-test';
+  imageData = [
+    {
+      id: '1',
+      img: 'assets/img/1.png',
+    },
+    {
+      id: '2',
+      img: 'assets/img/3.png',
+    },
+    {
+      id: '3',
+      img: 'assets/img/4.png',
+    },
+  ];
 
-  typedOptions_2: string[] = ['Auto Rickshaw', 'Cab', 'Tempo']
+  customOptions: OwlOptions = {
+		loop: true,
+		mouseDrag: true,
+		touchDrag: true,
+		pullDrag: true,
+		dots: false,
+		autoplay: true,
+		navSpeed: 50,
+		navText: ['', ''],
+		responsive: {
+			0: {
+				items: 1
+			},
+			400: {
+				items: 1
+			},
+			740: {
+				items: 1
+			},
+			940: {
+				items: 1
+			}
+		},
+		nav: false,
+		autoplayTimeout: 1500
+	}
+
+  typedOptions_2: string[] = ['Auto Rickshaw', 'Cab', 'Tempo'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxTypedJsModule } from 'ngx-typed-js';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgxTypedJsModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxTypedJsModule, AppRoutingModule, CarouselModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
